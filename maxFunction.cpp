a61e3b3b41f417e525e7fe3f5b14bb659caea0ef
mjones158 <127266863+mjones158@users.noreply.github.com>
2023-04-11 17:17:19 -0600

    Add files via upload

--- maxFunction.cpp
+++ maxFunction.cpp
@@ -53,5 +53,5 @@ void findMax(const string& filename) {
 int main(){
     //clock_t tStart = clock();
     findMax("CTU-13.csv");
-    //printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+    \\printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 }
